7adec68b21aea5a837f56c0c0cf490a91100f6ad
Marat <maratabdrahmanov@yandex.ru>
2022-08-31 09:26:57 +0300

    Реализовано вычисление точек пересеечения

--- src/calculations/nodal.py
+++ src/calculations/nodal.py
@@ -1,3 +1,7 @@
+from shapely.geometry import Polygon as SPolygon, Point, LineString
+
+from models.models import NodalCalcDecision
+
 
 def calc_nodal(vlp: dict, ipr: dict):
     """
@@ -10,6 +14,14 @@ def calc_nodal(vlp: dict, ipr: dict):
     ipr : dict
         Словарь, содержащий IPR
     """
+
+    # Populate R-tree index with bounds of grid cells
+    well = LineString(zip(vlp["q_liq"], vlp["p_wf"]))
+    plast = LineString(zip(ipr["q_liq"], ipr["p_wf"]))
+    point = well.intersection(plast)
+    print(point)
+    parse_point = NodalCalcDecision(p_wf = point.y, q_liq=point.x).dict()
+    return [parse_point]
     # Можно использовать numpy или библиотеку Shapely, LineString intersection
     pass
 

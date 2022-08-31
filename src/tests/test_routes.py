b03a108cd6c1e24ea52b8a0b7d4aef9551a5dea9
Marat <maratabdrahmanov@yandex.ru>
2022-08-31 10:48:11 +0300

    Добавлены тесты для проверки корректности вычислений nodal

--- src/tests/test_routes.py
+++ src/tests/test_routes.py
@@ -1,11 +1,64 @@
 import numpy as np
 import pytest
-
+import requests
+import json
 
 @pytest.fixture
 def generate_data_for_tests():
-    pass
+    return {
+  "vlp": {
+    "q_liq": [
+      0,
+      30,
+      60,
+      90,
+      120,
+      150
+    ],
+    "p_wf": [
+      200,
+      190,
+      180,
+      175,
+      185,
+      200
+    ]
+  },
+  "ipr": {
+    "q_liq": [
+      0,
+      30,
+      60,
+      90,
+      120,
+      150
+    ],
+    "p_wf": [
+      200,
+      180,
+      160,
+      140,
+      120,
+      100
+    ]
+  }
+}
 
-
+def test_connection(generate_data_for_tests):
+  response = requests.post(url="http://localhost:8003/nodal/calc",
+                           json=generate_data_for_tests)
+  assert response.status_code == 200
 def test_calc_model_success(api_client, generate_data_for_tests):
-    pass
+    response = requests.post(url="http://localhost:8003/nodal/calc",
+                             json=generate_data_for_tests)
+    answer = [
+  {
+    "p_wf": 200,
+    "q_liq": 0
+  }
+]
+    text = response.text
+    print()
+    text = json.loads(text)
+    #assert response.status_code == 200
+    assert text == answer
